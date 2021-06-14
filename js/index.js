8c5fce291b5fd524b050e958c9f4e570b9f928b8
victoriaCJ28 <victoria.akicastro92@gmail.com>
2021-06-14 13:42:19 -0600

    Se corriguen clases del menu, se agrega footer inicio formulario

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+// initialize carrousel slick
 $(document).ready(function () {
   $(".your-class").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
   });
 });
